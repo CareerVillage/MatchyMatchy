a22e8c3ee85c4f4e5fdd9dff2488a79d5b7c2cfa
Jared Chung <jared@careervillage.org>
2016-08-11 15:43:27 -0700

    Adds some folders and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='MatchyMatchy',
+    packages=['matchymatchy',],
+    version='0.1dev',
+    description='Match career advice requests to Professionals who can help',
+    url='https://github.com/CareerVillage/matchymatchy',
+    license='MIT',
+)
